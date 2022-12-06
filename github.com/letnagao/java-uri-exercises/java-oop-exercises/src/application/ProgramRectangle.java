a5d1a3317ddf048fe17414f922df677abf83dfed
letnagao <leticia-nagao@hotmail.com>
2022-12-05 21:18:55 -0300

    added program employee

---
+++ github.com/letnagao/java-uri-exercises/java-oop-exercises/src/application/ProgramRectangle.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rectangle;
+
+public class ProgramRectangle {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter rectangle width and height: ");
+		
+		Rectangle rectangle = new Rectangle();
+		
+		rectangle.width = sc.nextDouble();
+		rectangle.height = sc.nextDouble();
+
+		rectangle.Area();
+		System.out.println("AREA = " + rectangle.Area());
+		
+		rectangle.Perimeter();
+		System.out.println("PERIMETER = " + rectangle.Perimeter());
+		
+		rectangle.Diagonal();
+		System.out.println("DIAGONAL = " + rectangle.Diagonal());
+		sc.close();
+		
+	}
+
+}
